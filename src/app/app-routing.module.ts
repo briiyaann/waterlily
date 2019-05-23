6ffa47d2064b0d388d550535596b654a05dc9aa2
unknown <franzbrianbriones@gmail.com>
2019-05-23 19:54:55 +0800

    first commit with landing page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+	{
+		path: '',
+		loadChildren: './landing/landing.module#LandingPageModule'
+	},
+	{
+		path: 'home',
+		loadChildren: './home/home.module#HomePageModule'
+	},
+	{
+		path: 'list',
+		loadChildren: './list/list.module#ListPageModule'
+	},
+	{ path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' }
+];
+
+@NgModule({
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+	],
+	exports: [RouterModule]
+})
+export class AppRoutingModule { }
