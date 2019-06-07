d2fd95afbadbf7eaa742c5361bf86096f37935ba
unknown <franzbrianbriones@gmail.com>
2019-06-07 21:42:54 +0800

    added dining and accommodation pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,52 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html'
+	selector: 'app-root',
+	templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public appPages = [
-    {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
-    },
-    {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
-    }
-  ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+	branch = '';
+	appPages:any;
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+	constructor(
+		private platform: Platform,
+		private splashScreen: SplashScreen,
+		private statusBar: StatusBar,
+		private storage: Storage
+	) {
+		this.storage.get('branch').then((data) => {
+			this.branch = data;
+
+			this.appPages = [
+				{
+					title: 'Home',
+					url: '/home/' + this.branch,
+					icon: 'home'
+				},
+				{
+					title: 'Dining',
+					url: '/dining',
+					icon: 'restaurant'
+				},
+				{
+					title: 'Accommodation',
+					url: '/accommodation',
+					icon: 'bed'
+				}
+			];
+		});
+
+		this.initializeApp();
+	}
+
+	initializeApp() {
+		this.platform.ready().then(() => {
+			this.statusBar.styleDefault();
+			this.splashScreen.hide();
+		});
+	}
 }
